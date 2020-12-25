4cc4e6a233cd481e53ef050ad6a8343b0d6547d3
Raghav Kuppan <raghav.kuppan@gatech.edu>
2020-12-25 14:33:33 -0500

    Remove borders and Construct image

--- aoc20/src/main.rs
+++ aoc20/src/main.rs
@@ -39,6 +39,7 @@ struct Jigsaw {
     tiles: HashMap<u64, Vec<Vec<u8>>>,
     dimension: usize,
     num_tiles: usize,
+    image: Vec<Vec<u8>>,
 }
 
 impl Jigsaw {
@@ -47,8 +48,11 @@ impl Jigsaw {
     // 1 2 3    3 2 1
     // 4 5 6 => 6 5 4
     // 7 8 9    9 8 7
-    fn flip_90(&mut self, tile: u64) {
-        let current_tile = self.tiles.get_mut(&tile).unwrap();
+    fn flip_90(&mut self, tile: u64, flip_image: bool) {
+        let current_tile = match flip_image {
+            false => self.tiles.get_mut(&tile).unwrap(),
+            true => &mut self.image,
+        };
         for row in current_tile {
             row.reverse();
         }
@@ -57,36 +61,54 @@ impl Jigsaw {
     // 1 2 3    7 8 9
     // 4 5 6 => 4 5 6
     // 7 8 9    1 2 3
-    fn flip_180(&mut self, tile: u64) {
-        let current_tile = self.tiles.get_mut(&tile).unwrap();
+    fn flip_180(&mut self, tile: u64, flip_image: bool) {
+        let current_tile = match flip_image {
+            false => self.tiles.get_mut(&tile).unwrap(),
+            true => &mut self.image,
+        };
         current_tile.reverse();
     }
 
     // 1 2 3    7 4 1
     // 4 5 6 => 8 5 2
     // 7 8 9    9 6 3
-    fn rotate_forward(&mut self, val: u64) {
-        let tile = self.tiles.get(&val).unwrap();
+    fn rotate_forward(&mut self, val: u64, rotate_image: bool) {
+        let tile = match rotate_image {
+            false => self.tiles.get_mut(&val).unwrap(),
+            true => &mut self.image,
+        };
         let mut tmp_vec: Vec<Vec<u8>> = (0..tile[0].len())
             .map(|i| tile.iter().map(|v| v[i]).collect())
             .collect();
         for v in tmp_vec.iter_mut() {
             v.reverse();
         }
-        self.tiles.insert(val, tmp_vec);
+        match rotate_image {
+            false => self.tiles.insert(val, tmp_vec),
+            true => None,
+        };
     }
 
     // Reverse of rotate_forward
-    fn rotate_backward(&mut self, val: u64) {
-        self.flip_90(val);
-        let tile = self.tiles.get(&val).unwrap();
+    fn rotate_backward(&mut self, val: u64, rotate_image: bool) {
+        match rotate_image {
+            true => self.flip_90(0, true),
+            false => self.flip_90(val, false),
+        };
+        let tile = match rotate_image {
+            false => self.tiles.get_mut(&val).unwrap(),
+            true => &mut self.image,
+        };
         let mut tmp_vec: Vec<Vec<u8>> = (0..tile[0].len())
             .map(|i| tile.iter().map(|v| v[v.len() - 1 - i]).collect())
             .collect();
         for v in tmp_vec.iter_mut() {
             v.reverse();
         }
-        self.tiles.insert(val, tmp_vec);
+        match rotate_image {
+            false => self.tiles.insert(val, tmp_vec),
+            true => None,
+        };
     }
 
     fn helper(
@@ -135,15 +157,15 @@ impl Jigsaw {
                     None => (),
                 };
                 // Flip along horizontal axis
-                self.flip_180(*val);
+                self.flip_180(*val, false);
                 match self.helper(square, *val, i, j, unseen) {
                     Some(v) => return Some(v),
                     None => (),
                 };
                 // Flip back
-                self.flip_180(*val);
+                self.flip_180(*val, false);
                 // Rotate Forward
-                self.rotate_forward(*val);
+                self.rotate_forward(*val, false);
             }
             unseen.insert(*val);
             square[i][j] = 0;
@@ -189,10 +211,60 @@ impl Jigsaw {
         }
         up & left
     }
+
+    fn remove_borders(&mut self) {
+        for (val, tile) in self.tiles.iter_mut() {
+            // Remove last row
+            (*tile).pop();
+            // Remove first row
+            *tile = tile[1..].to_vec();
+            // Remove first and last column
+            let mut new_vec = Vec::new();
+            for i in 0..tile.len() {
+                let mut tmp_vec = Vec::new();
+                for j in 1..tile[0].len() - 1 {
+                    tmp_vec.push(tile[i][j]);
+                }
+                new_vec.push(tmp_vec);
+            }
+            *tile = new_vec;
+        }
+        for (_, tile) in self.tiles.iter() {
+            assert!(tile.len() == self.dimension - 2);
+            assert!(tile[0].len() == self.dimension - 2)
+        }
+    }
+
+    fn construct_image(&mut self, square: &Vec<Vec<u64>>) {
+        let new_dim = self.dimension - 2;
+        let mut image = vec![vec![0; new_dim * self.num_tiles]; new_dim * self.num_tiles];
+        for i in 0..square.len() {
+            for j in 0..square[0].len() {
+                let current_tile = self.tiles.get(&square[i][j]).unwrap();
+                //println!("Current square: {}", square[i][j]);
+                for k in 0..new_dim {
+                    for l in 0..new_dim {
+                        /*
+                        println!(
+                            "{} {} {} {} {}",
+                            i * new_dim + k,
+                            j * new_dim + l,
+                            k,
+                            l,
+                            current_tile[k][l]
+                        );
+                        */
+                        image[i * new_dim + k][j * new_dim + l] = current_tile[k][l];
+                    }
+                }
+            }
+        }
+        self.image = image;
+    }
 }
 
 fn main() {
-    let input = read_input("input").unwrap();
+    let input = read_input("ex1").unwrap();
     let input = get_parsed_input(input);
     let num_tiles = (input.len() as f64).sqrt() as usize;
     println!("Num_tiles: {}", num_tiles);
@@ -202,7 +274,27 @@ fn main() {
         tiles: input,
         num_tiles: num_tiles,
         dimension: 10,
+        image: vec![],
     };
-    let square = jigsaw.backtrack(0, 0, &mut square, &mut unseen);
+    let square = jigsaw.backtrack(0, 0, &mut square, &mut unseen).unwrap();
     println!("Part1: {:?}", square);
+    jigsaw.remove_borders();
+    jigsaw.construct_image(&square);
+    //println!("{:?}", jigsaw.image);
+    /*
+    let mut ex_image = read_input("ex1-image").unwrap();
+    let mut new_image = Vec::new();
+    for v in ex_image.into_iter() {
+        new_image.push(v.into_bytes());
+    }
+    println!("{:?}", jigsaw.tiles.get(&1171));
+    println!(
+        "{}",
+        jigsaw
+            .image
+            .iter()
+            .zip(new_image.iter())
+            .all(|(a, b)| a == b)
+    );
+    */
 }
